5809b275f9030f705504f85b6ebafce10f9e02d7
Yaroslav O. Kosmina <darviarush@mail.ru>
2023-09-29 19:11:19 +0300

    allocation

--- lib/Aion/View.pm
+++ lib/Aion/View.pm
@@ -14,23 +14,6 @@ BEGIN {
 # Time::HiRes::time() ?
 srand(time()+$$);
 
-use DDP {
-	colored => 1,
-	class => {
-		expand => "all",
-		inherited => "all",
-		show_reftype => 1,
-	},
-	deparse => 1,
-	show_unicode => 1,
-	show_readonly => 1,
-	print_escapes => 1,
-	#show_refcount => 1,
-	#show_memsize => 1,
-	caller_info => 1,
-	output => 'stdout',
-};
-
 # Импорт
 sub import {
 	my $pkg = caller;
@@ -42,47 +25,14 @@ use Aion::Carp;
 use Aion::Action::Util qw/msg trace/;
 use Aion::Fs;
 use Aion::Format;
+use Aion::Spirit;
 use Text::Trim qw/trim ltrim rtrim/;
 use List::Util qw/all any first reduce pairmap pairgrep/;
-
-*np = \\&DDP::np;
-*p = \\&DDP::p;
-*ASSERT = \\&Aion::View::ASSERT;
-*firstidx = \\&Aion::View::firstidx;
-*firstres = \\&Aion::View::firstres;
 
 with qw/Aion::Action Aion::Sige Aion::Run/;
 
 1" or die;
 
-}
-
-# assert
-sub ASSERT {
-	die "ASSERT: ".(ref $_[1]? $_[1]->(): $_[1])."\n" if !$_[0];
-}
-
-# Ищет в списке первое совпадение и возвращает индекс найденного элемента
-sub firstidx (&@) {
-	my $s = shift;
-
-	my $i = 0;
-	for(@_) {
-		return $i if $s->();
-		$i++;
-	}
-	return undef;
-}
-
-# Ищет в списке первый положительный разультат функции
-sub firstres (&@) {
-	my $s = shift;
-
-	for(@_) {
-		my $x = $s->();
-		return $x if $x;
-	}
-	return undef;
 }
 
 1;
